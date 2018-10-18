b3e149d555f899d8770feb4537df44f479473a1e
oleggator <oleg@utkin.xyz>
2018-10-19 01:20:00 +0300

    refactor reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -4,35 +4,23 @@ const MOCKBA = { lat: 55.751244, lng: 37.618423 };
 
 const initialState = {
   couriers: [],
-  highlightedCourierId: '',
-  center: { lat: 55.751244, lng: 37.618423 },
   center: MOCKBA,
 };
 
 const rootReducer = (state = initialState, action) => {
   switch (action.type) {
-    case actions.RECEIVE_COURIERS: {
-      return {
-        ...state,
-        couriers: action.couriers,
-      };
-    }
-    case actions.HIGHLIGHT_COURIER: {
-      return {
-        ...state,
-        highlightedCourierId: action.highlightedCourierId,
-      };
-    }
-
-    default: {
-      return state;
-    }
+    case actions.RECEIVE_COURIERS: return {
+      ...state,
+      couriers: action.couriers,
+    };
 
     case actions.PAN: return {
       ...state,
       center: action.location,
     };
 
+
+    default: return state;
   }
 };
 
