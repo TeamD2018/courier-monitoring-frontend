d579b77c737940646ee2feb29475599fa367eab7
oleggator <oleg@utkin.xyz>
2018-10-19 01:24:14 +0300

    fix sidebar css

--- src/components/sidebar.jsx
+++ src/components/sidebar.jsx
@@ -1,23 +1,40 @@
 import React, { PureComponent } from 'react';
 import styled from 'styled-components';
 import PropTypes from 'prop-types';
+import { Portal } from '@blueprintjs/core';
 
-const StyledDiv = styled.div`
+const StyledPortal = styled(Portal)`
+  display: flex;
+  flex-direction: column;
   overflow: hidden;
   position: fixed;
   z-index: 0;
   top: 0;
   left: 0;
   width: 20rem;
-  height: 100vh;
+  max-height: 100%;
   
-  padding: 1rem;
+  padding: 0.5rem;
+  
+  @media only screen and (max-device-width: 667px) 
+                     and (-webkit-min-device-pixel-ratio: 2)
+                     and (orientation: portrait) { 
+    width: 100%;
+  }
+  
+  @media only print {
+    display: none;
+  }
 `;
 
 class Sidebar extends PureComponent {
   render() {
     const { children } = this.props;
-    return <StyledDiv>{children}</StyledDiv>;
+    return (
+      <StyledPortal>
+        {children}
+      </StyledPortal>
+    );
   }
 }
 
