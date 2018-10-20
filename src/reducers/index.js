63a2ad2c520d83bdd2200065ec9e95b548af9607
Dnnd <ddann9@live.com>
2018-10-20 15:49:36 +0300

    add geo history fetch reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,9 @@
 import * as actions from '../actions';
 
-const MOCKBA = { lat: 55.751244, lng: 37.618423 };
+const MOCKBA = {
+  lat: 55.751244,
+  lng: 37.618423,
+};
 
 const initialState = {
   couriers: [],
@@ -10,38 +13,58 @@ const initialState = {
 
 const rootReducer = (state = initialState, action) => {
   switch (action.type) {
-    case actions.RECEIVE_COURIERS: return {
-      ...state,
-      couriers: action.couriers,
-    };
+    case actions.RECEIVE_COURIERS:
+      return {
+        ...state,
+        couriers: action.couriers,
+      };
 
-    case actions.SHOW_COURIERS_LIST: return {
-      ...state,
-      isCouriersListOpen: true,
-    };
+    case actions.SHOW_COURIERS_LIST:
+      return {
+        ...state,
+        isCouriersListOpen: true,
+      };
 
-    case actions.HIDE_COURIERS_LIST: return {
-      ...state,
-      isCouriersListOpen: false,
-    };
+    case actions.HIDE_COURIERS_LIST:
+      return {
+        ...state,
+        isCouriersListOpen: false,
+      };
 
-    case actions.PAN: return {
-      ...state,
-      center: action.location,
-    };
+    case actions.PAN:
+      return {
+        ...state,
+        center: action.location,
+      };
     case actions.RECEIVE_ORDERS: {
       const activeCourier = state.activeCourier || {};
       activeCourier.orders = action.order;
       console.log(...state);
       return {
         ...state,
+        activeCourier,
       };
     }
     case actions.RECEIVE_ORDERS_FAILED: {
       console.log('FAIL TO RECIEVE ORDERS');
       return { ...state };
     }
-    default: return state;
+    case actions.RECEIVE_GEO_HISTORY_FAILED: {
+      console.log('FAIL TO RECIEVE GEO HISTORY');
+      return { ...state };
+    }
+    case actions.RECEIVE_GEO_HISTORY: {
+      const activeCourier = state.activeCourier || {};
+      const geoHistory = activeCourier.geoHistory || [];
+      geoHistory.concat(action.geoHistory);
+      activeCourier.geoHistory = geoHistory;
+      return {
+        ...state,
+        activeCourier,
+      };
+    }
+    default:
+      return state;
   }
 };
 
