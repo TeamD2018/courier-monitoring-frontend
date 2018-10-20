90c0305595a82a218f7fe2c194a8a26bd633101e
Dnnd <ddann9@live.com>
2018-10-20 12:55:12 +0300

    add fetch recent orders method

--- src/sagas/index.js
+++ src/sagas/index.js
@@ -2,6 +2,7 @@ import { call, put, takeLatest } from 'redux-saga/effects';
 
 import { getCouriersByBoxField } from '../api';
 import { receiveCouriers, receiverCouriersFailed, REQUEST_COURIERS_BY_BOX_FIELD } from '../actions';
+import { fetchRecentOrders } from '../services/orders';
 
 function* couriersFetch(action) {
   try {
@@ -10,6 +11,15 @@ function* couriersFetch(action) {
     yield put(receiveCouriers(couriers));
   } catch (e) {
     yield put(receiverCouriersFailed());
+  }
+}
+
+function* ordersFetch(action) {
+  try {
+    const orders = yield call(fetchRecentOrders, action.courierId, action.period);
+    yield put(recieveOrders(orders));
+  } catch (e) {
+    yield put(recieveOrdersFailed());
   }
 }
 
