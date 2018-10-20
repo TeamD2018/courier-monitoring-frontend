90e616ff2a6bf9b7de6ea23e12ebe409cba47333
Dnnd <ddann9@live.com>
2018-10-20 15:49:03 +0300

    add geo history fetch saga

--- src/sagas/index.js
+++ src/sagas/index.js
@@ -1,13 +1,15 @@
 import { call, put, takeLatest } from 'redux-saga/effects';
 
-import { getCouriersByBoxField } from '../api';
+import { getCouriersByBoxField, getGeoHistory } from '../api';
 import {
   receiveCouriers,
   receiverCouriersFailed,
   receiveOrders,
   receiveOrdersFailed,
+  receiveGeoHistoryFailed,
+  receiveGeoHistory,
   REQUEST_COURIERS_BY_BOX_FIELD,
-  REQUEST_RECENT_ORDERS,
+  REQUEST_RECENT_ORDERS, REQUEST_GEO_HISTORY,
 } from '../actions';
 import { fetchRecentOrders } from '../services/index';
 
@@ -24,16 +26,27 @@ function* couriersFetch(action) {
 function* ordersFetch(action) {
   try {
     const orders = yield call(fetchRecentOrders, action.courierId, action.period);
-    console.log(orders)
+    console.log(orders);
     yield put(receiveOrders(orders));
   } catch (e) {
     yield put(receiveOrdersFailed());
   }
 }
 
+function* historyFetch(action) {
+  try {
+    const history = yield call(getGeoHistory, action.courierId, action.since);
+    console.log(history);
+    yield put(receiveGeoHistory(history));
+  } catch (e) {
+    yield put(receiveGeoHistoryFailed());
+  }
+}
+
 function* rootSaga() {
   yield takeLatest(REQUEST_COURIERS_BY_BOX_FIELD, couriersFetch);
   yield takeLatest(REQUEST_RECENT_ORDERS, ordersFetch);
+  yield takeLatest(REQUEST_GEO_HISTORY, historyFetch);
 }
 
 export default rootSaga;
