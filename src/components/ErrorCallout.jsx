bcdb3ebc615b3aca18123977d5a558d5bff602b1
oleggator <oleg@utkin.xyz>
2018-11-06 22:40:52 +0300

    redesign error callout

--- src/components/ErrorCallout.jsx
+++ src/components/ErrorCallout.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import styled from 'styled-components';
-import { Callout, Card, Intent } from '@blueprintjs/core';
+import {
+  Button, Callout, Card, Intent,
+} from '@blueprintjs/core';
 import { IconNames } from '@blueprintjs/icons';
 import PropTypes from 'prop-types';
 
@@ -15,13 +17,25 @@ const StyledCard = styled(Card)`
   width: 20rem;
 `;
 
+function reloadPage() { window.location.reload() }
+
 const ErrorCallout = ({ errorMessage, errorName }) => (
   <StyledCard elevation={2}>
-      {errorMessage}
     <Callout title={errorName} intent={Intent.DANGER} icon={IconNames.ERROR}>
+      <div>{errorMessage}</div>
+      <Button
+        large
+        minimal
+        intent={Intent.DANGER}
+        icon="refresh"
+        onClick={reloadPage}
+      >
+        Перезагрузить страницу
+      </Button>
     </Callout>
   </StyledCard>
 );
+
 
 ErrorCallout.propTypes = {
   errorMessage: PropTypes.string.isRequired,
